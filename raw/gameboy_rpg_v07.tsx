6f85f25d7a81a256e88116446aa84c95e696a041
Alexey Korepanov <alexkorep@gmail.com>
2020-12-08 21:48:06 +0100

    Add interaction with a stone

---
+++ raw/gameboy_rpg_v07.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="gameboy_rpg_v07" tilewidth="8" tileheight="8" tilecount="2048" columns="64">
+ <image source="gameboy_rpg_v07.png" width="512" height="256"/>
+</tileset>
