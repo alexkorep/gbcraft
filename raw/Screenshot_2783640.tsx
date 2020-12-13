447785cbcf1017b631e49999a9f7e6cfebdf20b8
Alexey Korepanov <alexkorep@gmail.com>
2020-12-13 13:32:23 +0100

    Add 2 maps

---
+++ raw/Screenshot_2783640.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Screenshot_2783640" tilewidth="8" tileheight="8" tilecount="1600" columns="40">
+ <image source="Screenshot_2783640.png" width="320" height="320"/>
+</tileset>
